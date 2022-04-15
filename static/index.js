a27869301bae8752c0d1a5b268055d567561de37
drtztx <91367142+drtztx@users.noreply.github.com>
2022-04-15 16:20:20 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -465,9 +465,9 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
+        if (cps <= 5) return '不想玩了，下把下把';
+        if (cps <= 8) return '我想~~笑啊';
+        if (cps <= 10)  return '啊~~我好烦啊~~';
         if (cps <= 15) return '您';
         return '人？';
     }
